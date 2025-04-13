33bf6f5047955299183c633fbdfaf1427b3c5224
soulayman <soulaymanjaa09@gmail.com>
2025-04-13 16:25:56 +0200

    fin

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
-
+import Proptesting from'./components/props'
+import State from './components/state'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      {/* <div>
+        <Proptesting name = 'soulayman'/>
+      </div> */}
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <State/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
